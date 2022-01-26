4e65ba2bcf2f21840c97d657731751d50c3bd69b
harman89 <timurnorm@mail.ru>
2022-01-26 16:59:22 +0500

    база

---
+++ attestation/model.py
@@ -0,0 +1,41 @@
+import datetime
+
+from flask import Flask
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import *
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'df9d9b8a053375dbae2758d00192748b77c1208ddd6e478c65b35e982c3c633b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+    id = Column(Integer(), primary_key=True)
+    login = Column(String(255), nullable=False)
+    last_name = Column(String(255), nullable=False)
+    first_name = Column(String(255), nullable=False)
+    mid_name = Column(String(255))
+    password_hash = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False)
+    surname = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
+    role = Column(String(255), nullable=False, default="teacher")
+    last_login_time = Column(DateTime, default=datetime.datetime.now())
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"user {self.username}"
+    
